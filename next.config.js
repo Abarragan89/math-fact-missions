c91c983c4ad6dff83803f4d50895275684d70079
Anthony Barragan <anthony.bar.89@gmail.com>
2022-10-21 16:49:03 -0700

    TEST

--- next.config.js
+++ next.config.js
@@ -3,30 +3,30 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/:path*'
+        source: 'https://math-fact-missions.herokuapp.com/api/:path*',
+        destination: 'http://localhost:3000/api/:path*'
       }
     ]
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Origin, X-Requested-With, Content-Type, Accept',
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
-          },
-        ],
-      },
-    ]
-  }
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       headers: [
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: '*',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value: 'Origin, X-Requested-With, Content-Type, Accept',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Credentials',
+  //           value: 'true',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // }
 }
