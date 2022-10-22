d448661749799859d16b21f6ba9172f01aeb4b78
Anthony Barragan <anthony.bar.89@gmail.com>
2022-10-21 17:34:47 -0700

    TEST

--- next.config.js
+++ next.config.js
@@ -3,29 +3,19 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
+        source: '*/api/:path*',
         destination: 'http://localhost:3000/api/:path*'
       }
     ]
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       headers: [
-  //         {
-  //           key: 'Access-Control-Allow-Origin',
-  //           value: '*',
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Headers',
-  //           value: 'Origin, X-Requested-With, Content-Type, Accept',
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Credentials',
-  //           value: 'true',
-  //         },
-  //       ],
-  //     },
-  //   ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ]
+      }
+    ]
   }
 }
