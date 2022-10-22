ab3a41b5d96b0544ddd69f66b463d19db859e65d
Anthony Barragan <anthony.bar.89@gmail.com>
2022-10-21 17:55:41 -0700

    TEST

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 // next.config.js
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*'
-      }
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://localhost:3000/api/:path*'
+  //     }
+  //   ]
+  // },
   async headers() {
     return [
       {
