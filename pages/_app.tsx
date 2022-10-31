d5c5b3a839e889a6b0b83e08db8c4cbdc98cad58
Anthony Barragan <anthony.bar.89@gmail.com>
2022-10-30 18:59:57 -0700

    Feature: rebuilt the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,12 +10,13 @@ function MyApp({ Component, pageProps }) {
       <meta charSet='UTF-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta name="description" content='Test your math fact knowledge and play math games to improve math fact fluency. Great for educational settings. Users do not need to create any account and their progress is saved on their device.' />
-      <meta name='keywords' content='math fact missions, math, games, educational games, elementary, student, teacher, fluency, multiplication, addition, subtraction, division, facts, fact'
+      <meta name='keywords' content='math fact missions, math fact mission, math, games, educational games, elementary, student, teacher, fluency, multiplication, addition, subtraction, division, facts, fact'
       />
       <meta name='og:title' property='og:title' content='Math Missions' />
       <meta property="og:type" content="website" />
       <meta property="og:description" content="Test your math fact knowledge and play math games to improve math fact fluency. Great for educational settings. Users do not need to create any account and their progress is saved on their device." />
       <meta property="og:image" content="/favicon.ico" />
+      <meta property="og:image:type" content="image/ico" />
       <link rel="shortcut icon" href="/favicon.ico"/>
 
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
